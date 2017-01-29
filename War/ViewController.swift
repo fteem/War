a9ffc58e2a2511de3c673cd138bed8bd7df8cd32
Ilija Eftimov <ileeftimov@gmail.com>
2017-01-29 02:49:44 +0100

    Done

--- War/ViewController.swift
+++ War/ViewController.swift
@@ -10,6 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    var rightScore = 0 {
+        didSet {
+           rightScoreLabel.text = String(rightScore)
+        }
+    }
+    
+    var leftScore = 0 {
+        didSet {
+            leftScoreLabel.text = String(leftScore)
+        }
+    }
+    
+    let cards = ["ace","card2","card3","card4","card5","card6","card7","card8",
+                 "card9","card10","jack","queen","king"]
+    
+    @IBAction func dealTapped(_ sender: UIButton) {
+        let leftCardValue = getRandomNumber()
+        let rightCardValue = getRandomNumber()
+        
+        let leftCardName = cards[leftCardValue]
+        let rightCardName = cards[rightCardValue]
+        
+        leftImageView.image = UIImage(named: leftCardName)!
+        rightImageView.image = UIImage(named: rightCardName)!
+        
+        if leftCardValue > rightCardValue {
+            leftScore += 1
+        } else if leftCardValue < rightCardValue {
+            rightScore += 1
+        } else {
+            // WAR!
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +58,10 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    private func getRandomNumber() -> Int {
+        return Int(arc4random_uniform(UInt32(cards.count)))
     }
 
 
